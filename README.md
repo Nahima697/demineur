6020c619121aa08fa6287efc6658ea2a7a692944
Nahima697 <nahima.toumi697@gmail.com>
2023-07-16 16:52:44 +0200

    génération de la grille en prenant en compte les rows et les cols

--- README.md
+++ README.md
@@ -8,5 +8,7 @@ Je compte dans un premier temps réaliser mon formulaire HTML en faisant choisir
 ### récupération des valeurs des inputs
 J'ai pu récupéré en ajoutant un écouteur sur le formulaire, les valeurs des inputs renseignés en console. Il faut que je puisse les utiliser dans l'api afin de générer la grille.
 
+J'ai pu récupérer les datas de l'api. Il faut maintenant que je puisse les utiliser pour générer la grille. J'ai pensé à faire une fonction generateGrid. J'ai reçu un nombre de tableau qui correspond au nombre de rows, et dans chaque tableau il y ' a un nombre de chiffre qui correspond aux cols. Ces chiffres sont des 0 et des 1, les 1 correspondants surement au mine. Je dois d'abord générer la grid en fonction du nombre de rows et cols.
+
 ## Deuxième étape
 Il faut générer la grille souhaitée grâce à un appel à une api.
--- main.js
+++ main.js
@@ -7,12 +7,12 @@ window.addEventListener('load', () => {
   form.addEventListener('submit', (e) => {
     e.preventDefault();
     const pseudo = e.target.pseudo.value;
-    const col = e.target.col.value;
-    const row = e.target.row.value;
+    const cols = e.target.cols.value;
+    const rows = e.target.rows.value;
     const mines = e.target.mines.value;
 
     // Je fais mon appel à l'API en utilisant les valeurs des inputs
-    let url = `https://minesweeper.js.apprendre-est.fun/generate_grid.php?rows=${row}&cols=${col}&mines=${mines}`;
+    let url = `https://minesweeper.js.apprendre-est.fun/generate_grid.php?rows=${rows}&cols=${cols}&mines=${mines}`;
     fetch(url)
       .then((response) => {
         if (!response.ok) {
@@ -21,8 +21,32 @@ window.addEventListener('load', () => {
         return response.json();
       })
       .then(function(data) {
-        console.log(data);
+        // let mines = data.mines;
+        generateGrid(rows, cols);
       });
   });
+
+  function generateGrid(rows, cols) {
+    let gridElement = document.createElement("div");
+    gridElement.classList.add("grid");
+
+    // Création des lignes
+    for (let i = 0; i < rows; i++) {
+      let rowElement = document.createElement("div");
+      rowElement.classList.add("row");
+
+      // Création des colonnes de chaque ligne
+      for (let j = 0; j < cols; j++) {
+        let colElement = document.createElement("div");
+        colElement.classList.add("col");
+        rowElement.appendChild(colElement);
+      }
+
+      gridElement.appendChild(rowElement);
+    }
+
+    // Ajouter la grille générée à la page
+    document.body.appendChild(gridElement);
+  }
 
 });
