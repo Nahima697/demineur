534113dedaccf1eb20d9732fc1f8642188622e84
Nahima697 <nahima.toumi697@gmail.com>
2023-07-16 16:14:34 +0200

    récupération des éléments dans l'url de la requete fetch, mais je fais face à une erreur 200  multi-origine Cors, le fetch apparemment n'accepte qu'un seule origine dans la requete

--- main.js
+++ main.js
@@ -1,18 +1,28 @@
 "use strict";
-window.addEventListener('load', ()=> {
+window.addEventListener('load', () => {
 
-    // je récupère les informations du formulaire et j'enregistre chaque valeur dans une constante
-    const form = document.querySelector('form');
+  // Je récupère les informations du formulaire et j'enregistre chaque valeur dans une constante
+  const form = document.querySelector('form');
 
-    form.addEventListener('submit', (e) => {
-        e.preventDefault()
-        const pseudo =  e.target.pseudo.value;
-        const col = e.target.col.value;
-        const row = e.target.row.value;
-        const mines = e.target.mines.value;
-        
-    });
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const pseudo = e.target.pseudo.value;
+    const col = e.target.col.value;
+    const row = e.target.row.value;
+    const mines = e.target.mines.value;
 
-})
+    // Je fais mon appel à l'API en utilisant les valeurs des inputs
+    let url = `https://minesweeper.js.apprendre-est.fun/generate_grid.php?rows=${row}&cols=${col}&mines=${mines}`;
+    fetch(url)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then(function(data) {
+        console.log(data);
+      });
+  });
 
-
+});
