af8db090fd79ac04e44c904306934570dc4df9d7
Nahima697 <nahima.toumi697@gmail.com>
2023-07-19 23:25:56 +0200

    j'ai réussi à skipper l'incrémentation des cases qui ont une bombe, seulement les autres cases qui sont égales à 1 donc qui sont à proximité d'une bombe prennent la classe mined

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 "use strict";
+
 window.addEventListener('load', () => {
 
-  // Je récupère les informations du formulaire et j'enregistre chaque valeur dans une constante
   const form = document.querySelector('form');
+  let newData;
+  let gridElement; // Declare gridElement globally
 
   form.addEventListener('submit', (e) => {
     e.preventDefault();
-    const pseudo = e.target.pseudo.value;
     const cols = e.target.cols.value;
     const rows = e.target.rows.value;
     const mines = e.target.mines.value;
 
-    // Je fais mon appel à l'API en utilisant les valeurs des inputs
     let url = `https://minesweeper.js.apprendre-est.fun/generate_grid.php?rows=${rows}&cols=${cols}&mines=${mines}`;
     fetch(url)
       .then((response) => {
@@ -21,17 +21,15 @@ window.addEventListener('load', () => {
         return response.json();
       })
       .then(function(data) {
-        const newData = adjoiningMines(rows, cols, data);
-        generateGrid(rows, cols, newData);
-        console.log(newData);
-      
+        newData = adjoiningMines(rows, cols, data); 
+        gridElement = generateGrid(rows, cols, newData);
+        document.body.appendChild(gridElement);
       });
   });
 
   function generateGrid(rows, cols, data) {
     let gridElement = document.createElement("div");
     gridElement.classList.add("grid");
-  
 
     // Création des lignes
     for (let i = 0; i < rows; i++) {
@@ -42,79 +40,99 @@ window.addEventListener('load', () => {
       for (let j = 0; j < cols; j++) {
         let colElement = document.createElement("div");
         colElement.classList.add("col");
-        rowElement.appendChild(colElement);
-     
 
-        // Vérifier s'il y a une mine à cette position : le data[i][j] cible la case
         if (data[i][j] === 1) {
-          colElement.classList.add("mined");
+          colElement.innerHTML = '<img src="image/bombe.png" width="50" height ="50" alt="Bombe">';
+        } else {
+          colElement.innerText = newData[i][j];
         }
+
+        rowElement.appendChild(colElement);
       }
 
       gridElement.appendChild(rowElement);
     }
 
-    // Ajouter la grille générée à la page
-    document.body.appendChild(gridElement);
-    adjoiningMines(rows, cols, data);
-  
-    gridElement.addEventListener('click', cellClicked);
+    gridElement.addEventListener('click', (e) => cellClicked(e, newData));
+    return gridElement;
   }
 
   function adjoiningMines(rows, cols, data) {
-    // Créer une nouvelle variable newData et copier les valeurs de data
-    let newData = data;
+    let newData = JSON.parse(JSON.stringify(data));
   
     for (let i = 0; i < rows; i++) {
       for (let j = 0; j < cols; j++) {
         if (data[i][j] === 1) {
-          // en haut
-          if (typeof newData[i - 1]?.[j] === "number") {
-            newData[i - 1][j]++;
-          }
-          // en haut à droite
-          if (typeof newData[i - 1]?.[j + 1] === "number") {
-            newData[i - 1][j + 1]++;
-          }
-          // à droite
-          if (typeof newData[i]?.[j + 1] === "number") {
-            newData[i][j + 1]++;
-          }
-          // en bas à droite
-          if (typeof newData[i + 1]?.[j + 1] === "number") {
-            newData[i + 1][j + 1]++;
-          }
-          // en bas
-          if (typeof newData[i + 1]?.[j] === "number") {
-            newData[i + 1][j]++;
-          }
-          // en bas à gauche
-          if (typeof newData[i + 1]?.[j - 1] === "number") {
-            newData[i + 1][j - 1]++;
-          }
-          // à gauche
-          if (typeof newData[i]?.[j - 1] === "number") {
-            newData[i][j - 1]++;
-          }
-          // en haut à gauche
-          if (typeof newData[i - 1]?.[j - 1] === "number") {
-            newData[i - 1][j - 1]++;
-          }
+         
+          continue;// permet de ne pas incrémenter les cases qui ont une bombe
+        }
+  
+        // en haut
+        if (typeof newData[i - 1]?.[j] === "number" && newData[i - 1][j] === 1) {
+          newData[i][j]++;
+        }
+  
+        // en haut à droite
+        if (typeof newData[i - 1]?.[j + 1] === "number" && newData[i - 1][j + 1] === 1) {
+          newData[i][j]++;
+        }
+  
+        // à droite
+        if (typeof newData[i]?.[j + 1] === "number" && newData[i][j + 1] === 1) {
+          newData[i][j]++;
+        }
+  
+        // en bas à droite
+        if (typeof newData[i + 1]?.[j + 1] === "number" && newData[i + 1][j + 1] === 1) {
+          newData[i][j]++;
+        }
+  
+        // en bas
+        if (typeof newData[i + 1]?.[j] === "number" && newData[i + 1][j] === 1) {
+          newData[i][j]++;
+        }
+  
+        // en bas à gauche
+        if (typeof newData[i + 1]?.[j - 1] === "number" && newData[i + 1][j - 1] === 1) {
+          newData[i][j]++;
+        }
+            
+        // à gauche
+        if (typeof newData[i]?.[j - 1] === "number" && newData[i][j - 1] === 1) {
+          newData[i][j]++;
+        }
+  
+        // en haut à gauche
+        if (typeof newData[i - 1]?.[j - 1] === "number" && newData[i - 1][j - 1] === 1) {
+          newData[i][j]++;
         }
       }
-    } 
+    }
+  
+    console.log("Data Array:");
+    console.table(data);
+    console.log("NewData Array:");
+    console.table(newData);
+  
     return newData;
   }
+  
 
-  function cellClicked (e) {
+  function cellClicked(e, data) {
     let cell = e.target;
-    if(e.button === 0) {
-      console.log('click gauche');
+    console.log(cell);
+
+    if (e.button === 0) {
+      const row = cell.parentNode.dataset.row;
+      const col = cell.dataset.col;
+      console.log(row, col);
+      cell.classList.remove('invisible');
+      console.log('Click gauche');
     }
-    if(e.button === 2) {
-      console.log('click droit');
+    if (e.button === 2) {
+      console.log('Click droit = drapeau');
     }
     console.log("Vous m'avez cliqué !");
-    console.log(cell);
+    console.log(data);
   }
 });
